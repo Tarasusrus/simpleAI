b2288fce29a627e4d28511351941c559575c15ab
Taras Malinovskii <bar.norilsk@gmail.com>
2025-10-13 13:36:16 +0300

    Add support for `Review` and `Commit` methods; refactor prompts configuration fields in `Config`.

--- pkg/llm/client.go
+++ pkg/llm/client.go
@@ -40,3 +40,41 @@ func (c *Client) Ask(prompt string) (string, error) {
 
 	return chatCompletion.Choices[0].Message.Content, nil
 }
+
+func (c *Client) Asksssss(prompt string) (string, error) {
+	var (
+		ctx = context.Background()
+	)
+	chatCompletion, err := c.api.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.SystemMessage(c.cfg.SysPrompt),
+			openai.UserMessage(prompt),
+		},
+		Model: openai.ChatModelGPT4_1Mini,
+	})
+	if err != nil {
+		c.l.Error("Failed to create chat", ErrChatCompletion.Error(), err)
+		return "", err
+	}
+
+	return chatCompletion.Choices[0].Message.Content, nil
+}
+
+func (c *Client) Asksssssssss(prompt string) (string, error) {
+	var (
+		ctx = context.Background()
+	)
+	chatCompletion, err := c.api.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.SystemMessage(c.cfg.SysPrompt),
+			openai.UserMessage(prompt),
+		},
+		Model: openai.ChatModelGPT4_1Mini,
+	})
+	if err != nil {
+		c.l.Error("Failed to create chat", ErrChatCompletion.Error(), err)
+		return "", err
+	}
+
+	return chatCompletion.Choices[0].Message.Content, nil
+}
